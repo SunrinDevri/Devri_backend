7c68da55a996c5b85e2537b30b85c68665b9a649
iwin2471 <iwin12372@gmail.com>
2017-11-16 20:38:13 +0900

    [modify] signin to passport

--- routes/auth.js
+++ routes/auth.js
@@ -15,15 +15,8 @@ module.exports = (router, Users, passport) =>{
       return res.status(200).json(result);
   })
 
-  .post('/signin', (req,res)=>{
-    var params = ['id', 'passwd'];
-    if(check_param(req.body, params)){
-      Users.findOne({id: req.body.id, passwd: req.body.passwd}, {__v:0, _id: 0, passwd: 0}, (err, user)=>{
-        if(err) return res.status(500).send("DB err");
-        if(user) return res.status(200).json(user);
-        else return res.status(404).send("incorrect id or passwd");
-      });
-    }else return res.status(400).send("param missing or null");
+  .post('/signin', passport.authenticate('local'), (req,res)=>{
+     return res.status(200).send(req.session.passport.user);
   })
 
   .get('/auto/:token', (req, res)=>{
