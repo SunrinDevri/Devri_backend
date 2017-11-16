2cb1ea15a9e921a034736dcc1949a108bdef33a0
iwin2471 <iwin12372@gmail.com>
2017-11-17 05:15:37 +0900

    [puiblic]

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,24 @@
 module.exports = (router, Users)=>{
   router.get('/', async (req, res)=>{
-    const users = await Users.find();
-    return res.status(200).json(users);
+    var today = now_time.format("YYYYMMDD");
+    var next_day = now_time.add(1, 'days');
+    var user = req.session.passport.user || "";
+    var user_info = await Users.findOne(user);
+    var todayPlan;
+    var nextdayPlan;
+
+    for(var i=0; i< user_info.calendar.length; i++){
+      if(user_info.calendar[i].date === today){
+        todayPlan = user_info.calendar[i].summary;
+      }else if(user_info.calendar[i].date === next_day){
+        nextdayPlan = user_info.calendar[i].summary;
+      }
+    }
+
+    user_info.calendar.todayPlan = todayPlan;
+    user_info.calendar.nextdayPlan = nextdayPlan;
+
+    return await res.render('index', {user: user_info});
   })
 
   .post('/user', (req, res)=>{
