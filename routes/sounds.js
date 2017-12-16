bb762643060569aa3520aedcad72560d1884856a
iwin2471 <iwin12372@gmail.com>
2017-12-16 11:01:38 +0900

    sounds update

--- routes/sounds.js
+++ routes/sounds.js
@@ -1,44 +1,29 @@
-var Q = require('q');
-var multer = require('multer');
-var imagePath = './sounds';
+import speech form '@google-cloud/speech';
 
-var upload = function (req, res) {
-  var deferred = Q.defer();
-  var storage = multer.diskStorage({
-    // 서버에 저장할 폴더
-    destination: function (req, file, cb) {
-      cb(null, imagePath);
-    },
+const client = new speech.SpeechClient();
 
-    // 서버에 저장할 파일 명
-    filename: function (req, file, cb) {
-      file.uploadedFile = {
-        name: file.mimetype.split('/')[0],
-        ext: file.mimetype.split('/')[1]
-      };
-      cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
-    }
-  });
 
-  var upload = multer({ storage: storage }).single('file');
-  upload(req, res, function (err) {
-    if (err) deferred.reject();
-    else if(req.file === undefined){
-      console.log("adsf");
-    }
-    else deferred.resolve(req.file.uploadedFile);
-  });
-  return deferred.promise;
+const config = {
+  languageCode: "ko",
+};
+
+const request = {
+  config: config,
+  audio: "",
 };
 
 
 module.exports = (router)=>{
-  router.post('/', function(req, res, next) {
-    upload(req, res).then(function (file) {
-     res.status(200).json(file);
-    }, function (err) {
-     res.status(500).send(err);
-   });
+  router.get('/', function(req, res, next) {
+    request.audio = req.params.base64;
+    client.recognize(request).then(data => {
+        const response = data[0];
+        const transcription = response.results.map(result => result.alternatives[0].transcript).join('\n');
+        console.log(`Transcription: `, transcription);
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
 });
 
 
